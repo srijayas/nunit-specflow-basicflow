e19acf7a2f105e2ade413cd811c11e70275d369d
Srijaya Suresh <srijayas@gmail.com>
2017-07-31 13:41:57 -0500

    implemented partial class for feature2

---
+++ nunit/SpecFlowFeature2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace nunit
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public partial class SpecFlowFeature2Feature : FeatureBase
+    {
+        public SpecFlowFeature2Feature(Platform type) : base(type)
+        {
+
+            Console.WriteLine("SpecFlowFeature2Feature constructor for " + type);
+        }
+    }
+}
+
